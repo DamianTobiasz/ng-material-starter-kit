5cf93ef1777f40561510d4037e94a028090268e6
damian <damiant12345@gmail.com>
2022-11-08 00:29:11 +0100

    feat: list of crypto chips implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CategoryListComponent } from './components/category-list/category-list.component';
+import { CryptoChipsListComponent } from './components/crypto-chips-list/crypto-chips-list.component';
 import { ProductListComponentModule } from './components/product-list/product-list.component-module';
 import { ProductsServiceModule } from './services/products.service-module';
 import { CategoryListComponentModule } from './components/category-list/category-list.component-module';
 import { CategoriesServiceModule } from './services/categories.service-module';
+import { CryptoChipsListComponentModule } from './components/crypto-chips-list/crypto-chips-list.component-module';
+import { CryptoServiceModule } from './services/crypto.service-module';
 
 @NgModule({
   imports: [
@@ -18,11 +21,17 @@ import { CategoriesServiceModule } from './services/categories.service-module';
         path: 'categories',
         component: CategoryListComponent,
       },
+      {
+        path: 'crypto',
+        component: CryptoChipsListComponent,
+      },
     ]),
     ProductListComponentModule,
     ProductsServiceModule,
     CategoryListComponentModule,
     CategoriesServiceModule,
+    CryptoChipsListComponentModule,
+    CryptoServiceModule,
   ],
   exports: [RouterModule],
 })
