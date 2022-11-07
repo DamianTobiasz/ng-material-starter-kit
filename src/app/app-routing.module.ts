66b6eaf6de26e3d70896990a4f9ab6e739f00d46
damian <damiant12345@gmail.com>
2022-11-08 00:08:40 +0100

    feat: list of categories implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
 import { ProductListComponentModule } from './components/product-list/product-list.component-module';
 import { ProductsServiceModule } from './services/products.service-module';
+import { CategoryListComponentModule } from './components/category-list/category-list.component-module';
+import { CategoriesServiceModule } from './services/categories.service-module';
 
 @NgModule({
   imports: [
-    RouterModule.forRoot([{
-      path: 'products',
-      component: ProductListComponent }]),
+    RouterModule.forRoot([
+      {
+        path: 'products',
+        component: ProductListComponent,
+      },
+      {
+        path: 'categories',
+        component: CategoryListComponent,
+      },
+    ]),
     ProductListComponentModule,
-    ProductsServiceModule
+    ProductsServiceModule,
+    CategoryListComponentModule,
+    CategoriesServiceModule,
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
